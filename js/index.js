14a3141f63090f45116f8b62344503fad8353835
Sasha <mix.filipov@mail.ru>
2019-06-18 18:43:09 -0700

    I am still working

--- js/index.js
+++ js/index.js
@@ -16,25 +16,34 @@ links.forEach(item => item.addEventListener('focus', (event) =>{
 
 //===============DRAG============
 
+// const h2 = document.querySelector('.intro h2');
+// h2.addEventListener('dragstart', (event) =>{
+//     event.stopPropagation();
+//     // h2.style.color = "orange"; // target doesn`t work here
+//     // event.currentTarget.style.color = 'orange'; //It works
+//     event.target.style.color = 'orange'; / in order for it to work the targets draggable property needs to be set to true!
+//     });
+
 const h2 = document.querySelector('.intro h2');
-h2.addEventListener('drag', (event) =>{
-    event.stopPropagation();
-    alert( "welcome" )
-    // event.target.style.color = "orange"; why it does not work
-    });
+h2.draggable = 'true';
+h2.addEventListener('dragstart', (event) =>{
+    event.target.style.color = 'orange';
+});
 
 //===============LOAD============
 
 const logo = document.querySelector('.logo-heading');
-logo.addEventListener('load', (event) => {
-    event.target.style.color = 'orange';
-});
+window.addEventListener('load', event =>  logo.style.color = "orange")
 
 //===============RESIZE============
 
-const logo = document.querySelector('.logo-heading');
-logo.addEventListener('load', (event) => {
-    event.target.style.color = 'orange';
-});
+window.addEventListener('resize', event => logo.textContent = "OOPS");
 
+//===============SELECT============
+
+const origin = document.querySelector('#origin');
+const copy = document.querySelector('#copy');
+origin.addEventListener('select', event =>
+    copy.textContent = event.target.value.substring(event.target.selectionStart,
+        event.target.selectionEnd));
 
